33aa77ab6ad625df5bb545b2c1c654467cde8f24
Jose Garza <jggarza5@gmail.com>
2020-02-28 13:02:22 -0600

    First commit

---
+++ src/build/precache-manifest.95f3100af45637c0f704555a4e537375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3505080547dc063a04112f993a6c46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2824a091eba0f651227",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "ceea66abca9edb2847ff",
+    "url": "/static/js/2.1e3d9ec8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1e3d9ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2824a091eba0f651227",
+    "url": "/static/js/main.ce24b12a.chunk.js"
+  },
+  {
+    "revision": "fc6b0834b26c11c7b958",
+    "url": "/static/js/runtime-main.742f9f3b.js"
+  }
+]);
